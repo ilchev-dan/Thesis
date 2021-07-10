1633effaf0d9444805b3aead2281b4dde93e0fc2
Ilchev Dan <ilchev_dan@outlook.com>
2021-07-10 15:34:38 +0300

    Added the display of tables, prepared windows for further work

---
+++ Thesis/EditorDB.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Thesis
+{
+    /// <summary>
+    /// Interaction logic for EditorDB.xaml
+    /// </summary>
+    public partial class EditorDB : Window
+    {
+
+        private readonly EquipmentContext _context = new();
+
+        private CollectionViewSource equipmentViewSource;
+
+        public EditorDB()
+        {
+            InitializeComponent();
+            equipmentViewSource = (CollectionViewSource)FindResource(nameof(equipmentViewSource));
+        }
+
+        private void itemBack_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _context.Equipments.Load();
+            equipmentViewSource.Source = _context.Equipments.Local.ToObservableCollection();
+            
+        }
+
+        private void itemSave_Click(object sender, RoutedEventArgs e)
+        {
+            _context.SaveChanges();
+            hereDataGrid.Items.Refresh();
+        }
+    }
+}
---
+++ Thesis/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace Thesis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEditDB_Click(object sender, RoutedEventArgs e)
+        {
+            EditorDB editorDBWindow = new EditorDB();
+            editorDBWindow.Show();
+            Close();
+        }
+
+        private void btnEditSheets_Click(object sender, RoutedEventArgs e)
+        {
+            EditorSheets editorSheetsWindow = new EditorSheets();
+            editorSheetsWindow.Show();
+            Close();
+        }
+    }
+}
