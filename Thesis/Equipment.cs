926af25fc3455c77d504a4dcb8a487a65484c10c
Ilchev Dan <ilchev_dan@outlook.com>
2021-07-17 22:51:05 +0300

    Fixed DB

--- Thesis/Equipment.cs
+++ Thesis/Equipment.cs
@@ -10,9 +10,10 @@ namespace Thesis
     {
         //Creating fields in the database
         public int EquipmentId { get; set; }
+        public string TypeEquip { get; set; }
         public string NameEquip { get; set; }
         public string DescriptionEquip { get; set; }
-        public int UnitEquip { get; set; }
+        public string UnitEquip { get; set; }
         public string Comment { get; set; }
 
         //Creating a one-to-many relationship with the "WorkplaceForEvent" table
---
+++ Thesis/Migrations/20210717194734_Fixed DB.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Thesis.Migrations
+{
+    public partial class FixedDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UnitEquip",
+                table: "Equipments",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddColumn<string>(
+                name: "TypeEquip",
+                table: "Equipments",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TypeEquip",
+                table: "Equipments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UnitEquip",
+                table: "Equipments",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+        }
+    }
+}
